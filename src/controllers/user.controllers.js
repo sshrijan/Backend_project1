3bfac1a8985eaaee24deeafeae60f5274b8d4fb9
Shrijan Shrestha <sshrijan65@gmail.com>
2025-01-21 17:39:59 +0545

    bug fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -126,7 +126,7 @@ const loginUser = asyncHandler(async(req, res) => {
     const {accessToken, refreshToken} = await 
     generateAccessAndRefreshTokens(user._id)
 
-    const loggedInUser = user.findById(_id)
+    const loggedInUser = user.findById(user._id)
     .select("-password","-refreshToken")
 
     const option{
